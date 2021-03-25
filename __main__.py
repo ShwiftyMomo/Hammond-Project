248cc6d4fa0b5666225da3dc005fe16a81f86c41
Milo_the_AI <milo@tacocat.com>
2021-03-25 12:58:29 -0700

    Added new graph/Folder containing graphs
    Added the graph of mean maximum value of SRB versus the size of the bridge, and a folder that has all of the graphs made concerning this project to date

--- __main__.py
+++ __main__.py
@@ -1,25 +1,12 @@
 #Import neccecary Bridge class and plotting class
 from Bridge import Bridge
 import matplotlib.pyplot as plt
-import time
 
+#Make a random bridge of size 20
+B=Bridge(size=20)
 
-for n in range(1,1000):
-    N=2*n
-    B=Bridge(size=N)
-    B.itter(reps=10000)
-    sampleSize=1000
-    maxes=0
-    for i in range(sampleSize):
-        B.itter(reps=100)
-        maxes+=B.maximum()
-
-    norm=maxes/sampleSize
-
-    print(str(N)+": "+str(norm))
-
-
-
+#Print the bridge
+print(B)
 
 
 
